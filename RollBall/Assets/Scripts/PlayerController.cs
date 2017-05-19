71d391e85c849ce3138728d71b0b567839352f14
yzhao334 <yzhao334@berkeley.edu>
2017-05-19 01:47:14 -0700

    to Moving the player.

---
+++ RollBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// physical update
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 vec = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(vec*speed);
+	}
+}
