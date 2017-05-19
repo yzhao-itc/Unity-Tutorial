daf8359bee4a0a78fbe006501dd87a5e05543eda
Yu Zhao <yzhao@us.toyota-itc.com>
2017-05-19 09:39:12 -0700

    Tutorial-during-script

---
+++ Roll-a-ball/Assets/scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    // initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // phyysical update
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
